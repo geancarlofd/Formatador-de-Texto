ecfc96a8d495ecd14d0e364e7e0fe0c9c7fe6e16
geancarlofd <geancarlofd@gmail.com>
2021-07-02 01:52:28 -0300

    Add Temas

--- js/index.js
+++ js/index.js
@@ -1,45 +1,78 @@
 window.onload = function () {
+
     //Chamada da funcao caso o botao for apertado
-    document.getElementById("bMaiusculo").onclick = function() {maiusculo()};//Maiusculo
-    document.getElementById("bMinusculo").onclick = function() { minusculo()};//Minusculo
+
+    document.getElementById("bMaiusculo").onclick = function () {//Maiusculo
+        maiusculo()
+    };
+
+    document.getElementById("bMinusculo").onclick = function () { //Minusculo
+        minusculo()
+    };
+
     document.getElementById("bPmaiusculo").onclick = function() {//Primeira Letra Maiuscula
         var txt = document.getElementById("txtBoxIpt");
         var txtAlterado = pMaiusculo(txt);
-        document.getElementById("dTxtBox").innerHTML = txtAlterado;
+        document.getElementById("txtBoxIpt").value = txtAlterado;
     };
     document.getElementById("bPminusculo").onclick = function() {//Primeira Letra Minuscula
         var txt = document.getElementById("txtBoxIpt");
         var txtAlterado = pMinuscula(txt);
-        document.getElementById("dTxtBox").innerHTML = txtAlterado;
+        document.getElementById("txtBoxIpt").value = txtAlterado;
     };
     document.getElementById("bAlternada").onclick = function() {//Texto ao Alternada
         var txt = document.getElementById("txtBoxIpt");
         var txtAlterado = textoAlternado(txt);
-        document.getElementById("dTxtBox").innerHTML = txtAlterado;
+        document.getElementById("txtBoxIpt").value = txtAlterado;
     };
     document.getElementById("bContrario").onclick = function() {//Texto ao Contrario
         var txt = document.getElementById("txtBoxIpt");
         var txtAlterado = textoContrario(txt);
-        document.getElementById("dTxtBox").innerHTML = txtAlterado;
+        document.getElementById("txtBoxIpt").value = txtAlterado;
     };
-    document.getElementById("bCopiar").onclick = function(){
+
+    document.getElementById("bCopiar").onclick = function () {//ClipBoard
         copiarTexto();
     }
-    document.getElementById("bLimpar").onclick = function(){
+    
+    document.getElementById("bLimpar").onclick = function(){//Limpar TextArea
         document.getElementById("txtBoxIpt").value = "";
-        document.getElementById("dTxtBox").innerHTML = "";
+        document.getElementById("txtBoxIpt").value = "";
+    }
+
+    //Buttons Tema
+    document.getElementById("btn-tema01").onclick = function(){
+        document.documentElement.style.setProperty('--cor-principal', '#2b729b');
+        document.documentElement.style.setProperty('--cor-principal-hover-1', '#2b9b88');
+        document.documentElement.style.setProperty('--cor-principal-hover-2', '#2b9b70');
+        document.documentElement.style.setProperty('--cor-principal-hover-3', '#2b589b');
+        document.documentElement.style.setProperty('--cor-principal-hover-4', '#562b9b');
+    }
+    document.getElementById("btn-tema02").onclick = function () {
+        document.documentElement.style.setProperty('--cor-principal', '#1e8562');
+        document.documentElement.style.setProperty('--cor-principal-hover-1', '#161616');
+        document.documentElement.style.setProperty('--cor-principal-hover-2', '#346751');
+        document.documentElement.style.setProperty('--cor-principal-hover-3', '#C84B31');
+        document.documentElement.style.setProperty('--cor-principal-hover-4', '#ECDBBA');
+    }
+    document.getElementById("btn-tema03").onclick = function () {
+        document.documentElement.style.setProperty('--cor-principal', '#5f5f5f');
+        document.documentElement.style.setProperty('--cor-principal-hover-1', '#CE2525');
+        document.documentElement.style.setProperty('--cor-principal-hover-2', '#FF6600');
+        document.documentElement.style.setProperty('--cor-principal-hover-3', '#F1C550');
+        document.documentElement.style.setProperty('--cor-principal-hover-4', '#76eec6');
     }
 
 };
 
 function maiusculo(){
     var txt = document.getElementById("txtBoxIpt");
-    document.getElementById("dTxtBox").innerHTML = txt.value.toUpperCase();
+    document.getElementById("txtBoxIpt").value = txt.value.toUpperCase();
 }
 
 function minusculo() {
     var txt = document.getElementById("txtBoxIpt");
-    document.getElementById("dTxtBox").innerHTML = txt.value.toLowerCase();
+    document.getElementById("txtBoxIpt").value = txt.value.toLowerCase();
 }
 
 function pMaiusculo(text) {
@@ -83,11 +116,12 @@ function textoAlternado(text){
 }
 
 function copiarTexto() {
-    var copyText = document.getElementById("dTxtBox");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
+    
+    let textArea = document.getElementById("txtBoxIpt");
+    textArea.select();
+    document.execCommand('copy');
+
+
 }
 
 function textoContrario(text) {
